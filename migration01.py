f5026a020a5443aff7466e6b6de25891b8c05f4f
liakyr <kyrre.liaaen@banenor.no>
2020-01-31 12:51:53 +0100

    Adding source data files Adding testing

---
+++ migration01.py
@@ -0,0 +1,13 @@
+import ciscoconfparse as ccp
+import re
+
+
+def parse_configuration(filename=None):
+    pass
+
+def parse_mac_table(filename=None):
+    with open("data/" + filename, "r") as
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_migration01.py
@@ -0,0 +1,3 @@
+import pytest
+from migration01 import parse_configuration, parse_mac_table
+
