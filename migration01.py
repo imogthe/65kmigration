1a09892e6f7b7534e15e82193d4d3f4ff790b670
liakyr <kyrre.liaaen@banenor.no>
2020-02-01 22:34:14 +0100

    Working on tests

--- migration01.py
+++ migration01.py
@@ -8,9 +8,26 @@ def parse_configuration(filename=None):
 def parse_mac_table(filename=None):
     if filename is None:
         raise(FileNotFoundError("No file name"))
-    with open("data/" + filename, "r") as mac_table_file:
-        pass
+    try:
+        with open("data/" + filename, "r") as mac_table_file:
+            return dict()
 
+    except FileNotFoundError:
+        exit(1)
+
+def extract_mac(mac_line = None):
+    if mac_line is None:
+        raise(ValueError)
+
+    extract = re.match(r"\*?\s+(?P<VLAN>\d+)\s+(?P<MAC>[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w+)\s+(\d+)\s+(?P<port>.+)", mac_line)
+
+    retval = {
+        "VLAN" : extract.group("VLAN"),
+        "MAC" : extract.group("MAC"),
+        "port" : extract.group("port")
+    }
+
+    return retval
 
 if __name__ == "__main__":
     main()
--- tests/test_migration01.py
+++ tests/test_migration01.py
@@ -1,9 +1,47 @@
 import pytest
-from migration01 import parse_configuration, parse_mac_table
+from migration01 import parse_configuration, parse_mac_table, extract_mac
 
-class test_parse_mac_table():
+class Test_parse_mac_table():
     def test_no_filename(self):
-        with raises(FileNotFoundError, match=r"No file name"):
+        with pytest.raises(FileNotFoundError, match=r"No file name"):
             retval = parse_mac_table()
-    # def test_returns_dict(self):
-    #     return_value = parse_mac_table()
+
+    def test_invalid_filename(self):
+        with pytest.raises(FileNotFoundError):
+            retval = parse_mac_table()
+
+    def test_returns_dict(self):
+        return_value = parse_mac_table("../data/dora.mac")
+        assert type(return_value) is dict
+
+class Test_extract_mac():
+    def test_extract_returns_dict(self):
+        test_string = "  3337  0050.56b8.0e42   dynamic  Yes        600   Po64"
+
+        return_value = extract_mac(test_string)
+
+        assert type(return_value) is dict
+
+    def test_extract_no_argument(self):
+        with pytest.raises(ValueError):
+            retval = extract_mac()
+
+    def test_extract_with_asterisk(self):
+        test_string = "*  274  984b.e172.5c5b   dynamic  Yes        600   Po20"
+
+        retval = extract_mac(test_string)
+        assert type(retval) is dict
+        assert type(retval["VLAN"]) is str
+        assert retval["VLAN"] == "274"
+        assert retval["MAC"] == "984b.e172.5c5b"
+        assert retval["port"] == "Po20"
+
+
+    def test_extract_without_asterisk(self):
+        test_string = "  3337  0050.56b8.0e42   dynamic  Yes        600   Po64"
+        retval = extract_mac(test_string)
+        assert type(retval) is dict
+        assert type(retval["VLAN"]) is str
+        assert retval["VLAN"] == "3337"
+        assert retval["MAC"] == "0050.56b8.0e42"
+        assert retval["port"] == "Po64"
