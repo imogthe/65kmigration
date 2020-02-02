2b70d47ff69f13439f2543e6987042624fdccd49
liakyr <kyrre.liaaen@banenor.no>
2020-02-02 21:35:05 +0100

    Working on tests

--- migration01.py
+++ migration01.py
@@ -1,6 +1,20 @@
 import ciscoconfparse as ccp
 import re
 
+def extract_vrf_definitions(config_file=None):
+    if config_file is None:
+        raise ValueError
+
+
+    config_parse = ccp.CiscoConfParse(config_file)
+
+    return_list = list()
+    for vrf_obj in config_parse.find_objects("^ip vrf"):
+        vrf_name = vrf_obj.re_match_typed("^ip vrf (.*)")
+        return_list.append(vrf_name)
+
+    return return_list
+
 
 def parse_configuration(filename=None):
     pass
@@ -9,8 +23,18 @@ def parse_mac_table(filename=None):
     if filename is None:
         raise(FileNotFoundError("No file name"))
     try:
-        with open("data/" + filename, "r") as mac_table_file:
-            return dict()
+        with open( filename, "r") as mac_table_file:
+            vlan_mac_table = dict()
+
+            for line in mac_table_file:
+                mac_details = extract_mac(line)
+
+                if not mac_details["VLAN"] in vlan_mac_table:
+                    vlan_mac_table[mac_details["VLAN"]] = dict()
+
+                vlan_mac_table[mac_details["VLAN"]][mac_details["MAC"]] = mac_details["port"]
+
+            return vlan_mac_table
 
     except FileNotFoundError:
         exit(1)
@@ -19,7 +43,7 @@ def extract_mac(mac_line = None):
     if mac_line is None:
         raise(ValueError)
 
-    extract = re.match(r"\*?\s+(?P<VLAN>\d+)\s+(?P<MAC>[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w+)\s+(\d+)\s+(?P<port>.+)", mac_line)
+    extract = re.match(r"\*?\s+(?P<VLAN>\d+|---)\s+(?P<MAC>[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w+)\s+(\d+|-)\s+(?P<port>.+)", mac_line)
 
     retval = {
         "VLAN" : extract.group("VLAN"),
@@ -28,6 +52,39 @@ def extract_mac(mac_line = None):
     }
 
     return retval
+
+def get_interface_names(config_file = None):
+    if config_file is None:
+        raise ValueError
+
+    config_parse = ccp.CiscoConfParse(config_file)
+
+    return_list = list()
+
+    for interface_obj in config_parse.find_objects("^interface"):
+        interface_name = interface_obj.re_match_typed("^interface (.*)")
+        return_list.append(interface_name)
+
+    return return_list
+
+def get_vrf_interfaces(config_file = None):
+    if config_file is None:
+        raise ValueError
+
+    config_parse = ccp.CiscoConfParse(config_file)
+
+def main():
+    vlan_mac_table = parse_mac_table("data/dora.mac")
+    for VLAN in vlan_mac_table:
+        print(VLAN)
+        for mac in vlan_mac_table[VLAN]:
+            print(f"\t{mac}")
+
+    vrf_list = extract_vrf_definitions("data/dora.confg")
+    print(vrf_list)
+
+    interface_list = get_interface_names("data/dora.confg")
+    print(interface_list)
 
 if __name__ == "__main__":
     main()
--- tests/test_migration01.py
+++ tests/test_migration01.py
@@ -1,5 +1,6 @@
 import pytest
-from migration01 import parse_configuration, parse_mac_table, extract_mac
+from migration01 import parse_configuration, parse_mac_table, extract_mac, extract_vrf_definitions
+from migration01 import get_interface_names, get_vrf_interfaces
 
 class Test_parse_mac_table():
     def test_no_filename(self):
@@ -11,7 +12,7 @@ class Test_parse_mac_table():
             retval = parse_mac_table()
 
     def test_returns_dict(self):
-        return_value = parse_mac_table("../data/dora.mac")
+        return_value = parse_mac_table("data/dora.mac")
         assert type(return_value) is dict
 
 class Test_extract_mac():
@@ -36,7 +37,6 @@ class Test_extract_mac():
         assert retval["MAC"] == "984b.e172.5c5b"
         assert retval["port"] == "Po20"
 
-
     def test_extract_without_asterisk(self):
         test_string = "  3337  0050.56b8.0e42   dynamic  Yes        600   Po64"
         retval = extract_mac(test_string)
@@ -45,3 +45,87 @@ class Test_extract_mac():
         assert retval["VLAN"] == "3337"
         assert retval["MAC"] == "0050.56b8.0e42"
         assert retval["port"] == "Po64"
+
+    def test_extract_no_port(self):
+        test_string = "* 3600  0100.5e48.7940    static  Yes          -   "
+        retval = extract_mac(test_string)
+        assert type(retval) is dict
+        assert type(retval["VLAN"]) is str
+        assert retval["VLAN"] == "3600"
+        assert retval["MAC"] == "0100.5e48.7940"
+        assert retval["port"] == " "
+
+    def test_extract_router_mac(self):
+        test_string = "*  ---  0000.0000.0000    static  No           -   Router"
+        retval = extract_mac(test_string)
+        assert type(retval) is dict
+        assert type(retval["VLAN"]) is str
+        assert retval["VLAN"] == "---"
+        assert retval["MAC"] == "0000.0000.0000"
+        assert retval["port"] == "Router"
+
+class Test_get_vrf_definitions():
+
+    def setup_method(self):
+        self.vrf_test_data_file = "tests/test_data/vrfs.txt"
+    def test_no_file(self):
+        with pytest.raises(ValueError):
+            retval = extract_vrf_definitions()
+
+    def test_invalid_file(self):
+        with pytest.raises(RuntimeError):
+            retval = extract_vrf_definitions("tjobing.txt")
+
+    def test_returns_list(self):
+        retval = extract_vrf_definitions(self.vrf_test_data_file)
+
+        assert type(retval) is list
+
+    def test_extract_vrf_list(self):
+        retval = extract_vrf_definitions(self.vrf_test_data_file)
+        desired_result = ["gprs-gb","gprs-gi","gprs-gn"]
+
+        assert all(
+            a == b for a,b in zip(retval, desired_result)
+        )
+
+class Test_get_interfaces():
+    def setup_method(self):
+        self.interface_config_file = "tests/test_data/interfaces.txt"
+
+    def test_no_config_file(self):
+        with pytest.raises(ValueError):
+            retval = get_interface_names()
+
+    def test_get_all_interfaces(self):
+        desired_result = [
+            "Port-channel1",
+            "Port-channel5",
+            "Loopback200",
+            "TenGigabitEthernet1/1/5",
+            "TenGigabitEthernet1/1/6",
+            "GigabitEthernet1/3/1",
+            "GigabitEthernet1/3/2",
+            "GigabitEthernet1/3/3",
+            "GigabitEthernet1/3/4"
+
+        ]
+
+        retval = get_interface_names(self.interface_config_file)
+
+        assert all(
+            a == b for a, b in zip(retval, desired_result)
+        )
+
+class Test_get_vrf_interfaces():
+    def setup_method(self):
+        self.config_file = "tests/test_data/vrf_interfaces.txt"
+
+    def test_no_file(self):
+        with pytest.raises(ValueError):
+            retval = get_vrf_interfaces()
+
+    def test_invalid_file(self):
+        with pytest.raises(RuntimeError):
+            retval = get_vrf_interfaces("jallaballa.txt")
+
